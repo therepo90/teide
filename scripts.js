1eba4c01e3da3c6a8d4b6cabe89d34853ad20af5
therepo90 <therepo90@gmail.com>
2024-02-11 20:45:31 +0100

    fffff

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// JavaScript do zwiększania licznika zadowolonych klientów, gdy element jest widoczny
+let satisfiedCustomers = 0;
+const targetSatisfiedCustomers = 200;
+
+function updateSatisfiedCounter() {
+    document.getElementById('satisfiedCounter').innerText = satisfiedCustomers;
+    satisfiedCustomers += 1;
+    if (satisfiedCustomers <= targetSatisfiedCustomers) {
+        requestAnimationFrame(updateSatisfiedCounter);
+    } else {
+        document.getElementById('satisfiedCounter').innerText = targetSatisfiedCustomers + "+";
+    }
+}
+
+// Intersection Observer do uruchamiania licznika, gdy pojemnik staje się widoczny
+const satisfiedContainer = document.getElementById('satisfiedContainer');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log('in viewport');
+            updateSatisfiedCounter();
+            satisfiedContainer.style.opacity = 1; // Spraw, aby pojemnik był widoczny
+            observer.unobserve(entry.target); // Przestań obserwować, gdy licznik zacznie się
+        }
+    });
+}, { threshold: 0.5 }); // Dostosuj próg według potrzeb
+
+// Rozpocznij obserwowanie pojemnika
+observer.observe(satisfiedContainer);
+
+// Dodaj funkcję do zmiany języka
+function changeLanguage(lang) {
+    let newUrl;
+    if (lang === 'en') {
+        newUrl = '/index.html';
+    } else if (lang === 'pl') {
+        newUrl = '/index-pl.html';
+    }
+    window.location.href = newUrl;
+}
