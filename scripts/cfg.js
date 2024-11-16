1821377bea8152587f4c37295f938737b90e13b8
therepo90 <therepo90@gmail.com>
2024-11-16 17:46:07 +0100

    zz

---
+++ scripts/cfg.js
@@ -0,0 +1,23 @@
+// pro cfg
+
+const env = {
+    DEV: {
+        API: 'http://localhost:7801',
+    },
+    PROD: {
+        API: 'https://api.teidepermit.eu',
+    },
+};
+
+
+let cfg;
+if (process.env.NODE_ENV === 'development') {
+    cfg = env.DEV;
+} else if (process.env.NODE_ENV === 'production') {
+    cfg = env.PROD;
+}else{
+    cfg = env.DEV;
+}
+
+export const config = cfg;
+console.log('env', {env: process.env.NODE_ENV, config});
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 // JavaScript do zwiększania licznika zadowolonych klientów, gdy element jest widoczny
+import {config} from "./cfg";
+
 let satisfiedCustomers = 0;
 const targetSatisfiedCustomers = 350;
 
@@ -27,3 +29,90 @@ const observer = new IntersectionObserver((entries) => {
 
 // Rozpocznij obserwowanie pojemnika
 observer.observe(satisfiedContainer);
+
+// on content laoded
+
+function openTripModal (trip, lang) {
+    //console.log({a:this, arguments})
+    console.log('opening modal');
+    if(trip==='teide' && lang==='en') {
+        document.getElementById('modal_teide_en').getElementsByClassName('modal-checkbox')[0].checked = true;
+    }
+    if(trip==='teide' && lang==='pl') {
+        document.getElementById('modal_teide_pl').getElementsByClassName('modal-checkbox')[0].checked = true;
+    }
+}
+
+console.log({config})
+function initModalStuff(btnId, modalId, target, lang) {
+    // Initialize Flatpickr for date fields
+    document.getElementById(btnId).addEventListener('click', (e) => {
+        e.preventDefault();
+        openTripModal(target, lang);
+    });
+    const modal = document.getElementById(modalId);
+    const startDate = modal.querySelector(`input[name="startDate"]`)
+    const endDate = modal.querySelector(`input[name="endDate"]`)
+    console.log({modalId, startDate, endDate})
+    flatpickr(startDate, {dateFormat: "Y-m-d"});
+    flatpickr(endDate, {dateFormat: "Y-m-d"});
+
+    // Handle form submission
+    console.log({modal})
+    modal.getElementsByClassName('modal-payment-btn')[0].addEventListener('click', async (e) => {
+        e.preventDefault(); // Prevent default form submission
+        const form = modal.getElementsByClassName('modalForm')[0];
+        const formData = new FormData(form);
+        // check if form is valid
+        if (!form.checkValidity()) {
+            alert('Please fill out all fields');
+            return;
+        }
+
+        // Convert FormData to an object
+        const data = Object.fromEntries(formData.entries());
+        //data.startDate = new Date(data.startDate);
+        //data.endDate = new Date(data.endDate);
+        const apiBase = config.API;
+
+        try {
+            let targetUrl =  `${apiBase}/api/teide-add`;
+            switch (target) {
+                case 'teide':
+                    targetUrl = lang==='pl' ? `${apiBase}/api/teide-add`: `${apiBase}/api/teide-eng-add`;
+                    break;
+                default:
+                    throw new Error('Invalid target');
+            }
+            const response = await fetch(targetUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (response.ok) {
+                const resJson = await response.json();
+                window.location.href = resJson.url;
+                //e.target.reset(); // Reset form after successful submission
+            } else {
+                // get error message
+                const errorRes = await response.json();
+                alert('Failed to submit the form: error: ' + errorRes.error);
+            }
+        } catch (error) {
+            console.error('Error submitting form:', error);
+            alert('An error occurred while submitting the form.');
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    if(window.location.href.indexOf('index-pl.html') > -1) {
+        initModalStuff('teideTripPL', 'modal_teide_pl', 'teide', 'pl');
+    }else{
+        initModalStuff('teideTripEN', 'modal_teide_en', 'teide', 'en');
+        initModalStuff('teideTripPL', 'modal_teide_pl', 'teide', 'pl');
+    }
+});
